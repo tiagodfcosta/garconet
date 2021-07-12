3be8bfe2a36c136db31e8873accd9806abb6c42c
danidash182 <danivenezza@gmail.com>
2021-07-12 17:13:50 +0100

    cenas

--- backend/db.js
+++ backend/db.js
@@ -105,7 +105,6 @@ export async function findProducts() {
     return res
 }
 
-//parcialmente correto, verificar como faz o update
 export async function updateTray(info) {
     const collection = await getCollection(DB_GARCONET, "bandeja");
 
@@ -121,7 +120,7 @@ export async function updateTray(info) {
         //a tray se torna a bandeja que foi criada
         tray = await collection.findOne({ aberta: true })
     }
-
+    //checa se o artigo existe, se sim, atualiza a quantidade e o valor, se não, manda tudo pra dentro
     let item = tray.artigos.find((a) => a.nome === info.nome)
     if (item) {
         item.quantidade += info.quantidade
@@ -129,7 +128,7 @@ export async function updateTray(info) {
     } else {
         tray.artigos.push(info)
     }
-
+    //faz um update na collection com as mudanças acima
     await collection.updateOne({
         _id: tray._id
     }, {
@@ -138,9 +137,5 @@ export async function updateTray(info) {
         }
     })
 
-    // const res1 = await collection.findOne({aberta: true});
-    // const res = await collection.updateOne(
-    //         {nome: info.nome}, {$inc: {quantidade: + info.quantidade, valor: + info.valor}}, {upsert: true}
-    // );
     return tray;
 }
